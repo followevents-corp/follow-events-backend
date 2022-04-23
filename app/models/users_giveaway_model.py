807b542f8d46f02ca3f7d6c261ea7dc619997c2d
ErickDevMaster <123skatesk8@gmail.com>
2022-04-23 16:24:32 -0400

    fix: changed column type user_id, giveaway_id to string and nullable false

--- app/models/users_giveaway_model.py
+++ app/models/users_giveaway_model.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 
 users_giveaway = db.Table('users_giveaway',
     db.Column('id', UUID(as_uuid = True), default=uuid4(), primary_key=True),
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('giveaway_id', db.Integer, db.ForeignKey('giveaway.id'))
+    db.Column('user_id', db.String, db.ForeignKey('user.id'), nullable=False),
+    db.Column('giveaway_id', db.String, db.ForeignKey('giveaway.id'), nullable=False)
 )
     
