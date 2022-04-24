4d6cba0a7d74c78267e2fa9608026a7b196cf835
Dorival Machado <dorivalmachado@outlook.com>
2022-04-24 18:24:43 -0300

    fix(request_data_exceptions): add resposnse and status_code

--- app/exceptions/request_data_exceptions.py
+++ app/exceptions/request_data_exceptions.py
@@ -1,3 +1,4 @@
 class MissingAttributeError(Exception):
-    def __init__(self, missing_keys: list):
-        self.missing_keys = missing_keys
+    def __init__(self, missing_keys: list, status_code=400):
+        self.response = {"missing_keys": missing_keys}
+        self.status_code = status_code
