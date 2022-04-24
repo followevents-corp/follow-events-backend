60cd157b73816fc6679c56a831d78655520659cb
Dorival Machado <dorivalmachado@outlook.com>
2022-04-24 17:56:33 -0300

    fix(user_exceptions): add default message and status code

--- app/exceptions/user_exceptions.py
+++ app/exceptions/user_exceptions.py
@@ -1,3 +1,5 @@
 class EmailFormatError(Exception):
-    def __init__(self):
-        self.message = "Provide a valid email"
+    def __init__(self, message="Provide a valid email", status_code=400):
+        self.message = message
+        self.status_code = status_code
+
