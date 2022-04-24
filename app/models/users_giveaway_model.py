b17098e4965ad394933cc25324e4a597db97e15e
ErickDevMaster <123skatesk8@gmail.com>
2022-04-24 13:39:19 -0400

    fix: changed the type of user_id and giveaway_id columns to UUID

--- app/models/users_giveaway_model.py
+++ app/models/users_giveaway_model.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 
 users_giveaway = db.Table('users_giveaway',
     db.Column('id', UUID(as_uuid = True), default=uuid4(), primary_key=True),
-    db.Column('user_id', db.String, db.ForeignKey('user.id'), nullable=False),
-    db.Column('giveaway_id', db.String, db.ForeignKey('giveaway.id'), nullable=False)
+    db.Column('user_id', UUID(as_uuid = True), db.ForeignKey('user.id'), nullable=False),
+    db.Column('giveaway_id', UUID(as_uuid = True), db.ForeignKey('giveaway.id'), nullable=False)
 )
     
