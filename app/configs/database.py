67cd57191079ad496372728fcb01c0da0a903994
Dorival Machado <dorivalmachado@outlook.com>
2022-04-25 08:40:57 -0300

    feat(database): structure models imports

--- app/configs/database.py
+++ app/configs/database.py
@@ -8,5 +8,11 @@ def init_app(app: Flask):
     db.init_app(app)
     app.db = db
 
-    from app.models.schedule_table import schedule_table
     from app.models.user_model import User
+    # importar Events model
+    from app.models.categories_model import Categories
+    from app.models.comment_model import Comment
+    # importar Giveaway model
+    from app.models.categories_events_table import events_categories
+    from app.models.schedule_table import schedule_table
+    from app.models.users_giveaway_model import users_giveaway
