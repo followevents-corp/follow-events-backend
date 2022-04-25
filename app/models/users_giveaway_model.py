826b86d8cd898239b8ec0bc95fcfda3acf67d08b
ErickDevMaster <123skatesk8@gmail.com>
2022-04-25 11:46:19 -0400

    fix: put the on delete cascade inside the foreign key

--- app/models/users_giveaway_model.py
+++ app/models/users_giveaway_model.py
@@ -4,8 +4,8 @@ from uuid import uuid4
 
 users_giveaway = db.Table('users_giveaway',
     db.Column('id', UUID(as_uuid = True), default=uuid4(), primary_key=True),
-    db.Column('user_id', UUID(as_uuid = True), db.ForeignKey('users.id'), ondelete='CASCADE', nullable=False),
-    db.Column('giveaway_id', UUID(as_uuid = True), db.ForeignKey('giveaway.id'), ondelete='CASCADE', nullable=False)
+    db.Column('user_id', UUID(as_uuid = True), db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False),
+    db.Column('giveaway_id', UUID(as_uuid = True), db.ForeignKey('giveaway.id', ondelete='CASCADE'), nullable=False)
 )
     
  
