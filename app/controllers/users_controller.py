1b48f097ad2804c22f2d39958e26f841bf03df0c
Cristian Tacca <christacca@gmail.com>
2022-04-26 13:28:07 -0300

    feat: treated DataError and others exceptions.

--- app/controllers/users_controller.py
+++ app/controllers/users_controller.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 from flask import jsonify, request
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, DataError
 from app.exceptions.user_exceptions import EmailFormatError
 from app.configs.database import db
 from app.models.user_model import User
@@ -19,6 +19,8 @@ def create_user():
 
     try:
         new_user = User(**data)
+    except TypeError as e:
+        return {'error': f'{e}'}, HTTPStatus.BAD_REQUEST
     except EmailFormatError:
         return {'error': f'Email format not acceptable: {data["email"]}, try ex.: your_mail@your_provider.com'}, HTTPStatus.BAD_REQUEST
 
@@ -32,6 +34,12 @@ def create_user():
         if 'Key (email)' in error:
             return {'error': 'Email already exists.'}, HTTPStatus.CONFLICT
         return {'error': 'Username already exists.'}, HTTPStatus.CONFLICT
+    except DataError as e:
+        session.rollback()
+        error = str(e)
+        if 'varying(30)' in error:
+            return {'error': 'Username has to be 6 to 30 characters.'}, HTTPStatus.BAD_REQUEST
+        return {'error': 'Name has to be less than 100 characters. If your name is greater than that, try abbreviate it. :D'}, HTTPStatus.BAD_REQUEST
 
     return jsonify(new_user), HTTPStatus.CREATED
 
