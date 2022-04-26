00dfc893f0a149aeae62ad5a2dd6159fd1a93fa5
Cristian Tacca <christacca@gmail.com>
2022-04-25 22:41:32 -0300

    feat: structure for user controller to complete users routes

--- app/routes/users_blueprint.py
+++ app/routes/users_blueprint.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
+from app.controllers.users_controller import create_user, get_user, update_user, delete_user
 
 bp = Blueprint('bp_users', __name__, url_prefix='/users')
 
-bp.post('')()
-bp.get('/<int:user_id>')()
-bp.patch('/<int:user_id>')()
-bp.delete('/<int:user_id>')()
+bp.post('')(create_user)
+bp.get('/<int:user_id>')(get_user)
+bp.patch('/<int:user_id>')(update_user)
+bp.delete('/<int:user_id>')(delete_user)
