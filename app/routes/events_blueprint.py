08b93dd22dcbaa318b0661156d431d32314c0bb6
sarah <sarahscardini@gmail.com>
2022-04-26 18:43:34 -0300

    fix: id type of events routes

--- app/routes/events_blueprint.py
+++ app/routes/events_blueprint.py
@@ -5,7 +5,7 @@ from app.controllers import events_controller
 bp = Blueprint("user", __name__, url_prefix="/events")
 
 bp.get("")(events_controller.get_events)
-bp.get("<str:events_id>")(events_controller.get_by_id_event)
+bp.get("<events_id>")(events_controller.get_by_id_event)
 bp.post("")(events_controller.create_event)
-bp.patch("<str:events_id>")(events_controller.update_event)
-bp.delete("<str:events_id>")(events_controller.del_event)
+bp.patch("<events_id>")(events_controller.update_event)
+bp.delete("<events_id>")(events_controller.del_event)
