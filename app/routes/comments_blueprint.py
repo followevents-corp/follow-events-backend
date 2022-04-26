ae1e7293dc8a0570c9424cb706a6694397e2004a
ErickDevMaster <123skatesk8@gmail.com>
2022-04-26 10:11:33 -0400

    feat: created the comment route

---
+++ app/routes/comments_blueprint.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from app.controllers import comment_controller 
+
+bp = Blueprint('comments', __name__, url_prefix='/events/<event_id>/comments')
+
+bp.post('')(comment_controller.create_comment)
+bp.get('')(comment_controller.get_comment)
+bp.patch('/<comment_id>')(comment_controller.update_comment)
+bp.delete('/<comment_id>')(comment_controller.delete_comment)
