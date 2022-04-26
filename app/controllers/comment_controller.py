df994bd7c5aebb63d1f68d1a74379ca9d32eb14c
ErickDevMaster <123skatesk8@gmail.com>
2022-04-26 11:25:38 -0400

    feat: created the function that adds a comment to the database

--- app/controllers/comment_controller.py
+++ app/controllers/comment_controller.py
@@ -1,5 +1,38 @@
+from http import HTTPStatus
+from app.exceptions.request_data_exceptions import AttributeTypeError, MissingAttributeError
+from app.models.comment_model import Comment
+from app.configs.database import db
+from flask import jsonify, request
+
+from app.services.general_services import check_keys, check_keys_type
+
+session = db.session
+
+
+def save_changes(new_comment):
+    session.add(new_comment)
+    session.commit()
+
+
 def create_comment():
-    pass
+    comment_data = request.get_json()
+    try:
+        verified_key = check_keys(comment_data, ['user_id', 'comment'])
+        check_keys_type(verified_key, {'user_id': str, 'comment': str})
+
+    except MissingAttributeError as e:
+        return e.response, e.status_code
+    except AttributeTypeError as e:
+        return e.response, e.status_code
+
+    new_comment = Comment(**verified_key)
+
+    save_changes(new_comment)
+
+    return jsonify({
+        "user_id": new_comment.user_id,
+        "comment": new_comment.comment,
+    }), HTTPStatus.CREATED
 
 
 def get_comment(comment_id: str, event_id: str):
