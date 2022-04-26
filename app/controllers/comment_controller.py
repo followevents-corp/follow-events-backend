b55e09887b51c73c690bdbac7250e7a79b638554
ErickDevMaster <123skatesk8@gmail.com>
2022-04-26 09:51:02 -0400

    feat: created the base functions of the comments controller

---
+++ app/controllers/comment_controller.py
@@ -0,0 +1,14 @@
+def create_comment():
+    pass
+
+
+def get_comment(comment_id: str, event_id: str):
+    pass
+
+
+def update_comment(comment_id: str, event_id: str):
+    pass
+
+
+def delete_comment(comment_id: str, event_id: str):
+    pass
