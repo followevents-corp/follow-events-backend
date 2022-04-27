8ecc2660a2f89f0a0af03382dd84d95ff939d483
Dorival Machado <dorivalmachado@outlook.com>
2022-04-27 20:24:08 -0300

    feat(schedule_controller): create controller to delete schedule

--- app/controllers/schedule_controller.py
+++ app/controllers/schedule_controller.py
@@ -1,11 +1,12 @@
+from dataclasses import asdict
 from http import HTTPStatus
+from multiprocessing import Event
 
 from flask import jsonify, request
 from sqlalchemy.orm.session import Session
 
 from app.configs.database import db
 from app.exceptions.invalid_id_exception import InvalidIdError
-
 from app.exceptions.request_data_exceptions import (
     AttributeTypeError,
     MissingAttributeError,
@@ -14,11 +15,10 @@ from app.models.events_model import Events
 from app.models.schedule_model import Schedule
 from app.models.user_model import User
 from app.services.events_services import get_additonal_information_of_event
-
 from app.services.general_services import check_keys, check_keys_type
 from app.services.invalid_id_services import check_id_validation
 from app.services.verify_values import incoming_values
-from dataclasses import asdict
+
 
 def create_schedule(user_id):
     data = request.get_json()
@@ -48,15 +48,11 @@ def create_schedule(user_id):
     event_id = new_data.get("event_id")
 
     existing_schedule = (
-        session.query(Schedule)
-        .filter_by(user_id=user_id, event_id=event_id)
-        .first()
+        session.query(Schedule).filter_by(user_id=user_id, event_id=event_id).first()
     )
 
     if existing_schedule:
-        return {
-            "error": "Event already added to user's schedule"
-        }, HTTPStatus.CONFLICT
+        return {"error": "Event already added to user's schedule"}, HTTPStatus.CONFLICT
 
     new_data.update({"user_id": user_id})
     new_schedule = Schedule(**new_data)
@@ -78,14 +74,39 @@ def get_schedule(user_id):
     except InvalidIdError as e:
         return e.response, e.status_code
 
-    scheduled_events_users = session.query(Events).select_from(Events).join(Schedule).filter(Schedule.user_id==user_id).all()
+    scheduled_events_users = (
+        session.query(Events)
+        .select_from(Events)
+        .join(Schedule)
+        .filter(Schedule.user_id == user_id)
+        .all()
+    )
 
     dict_events = [asdict(event) for event in scheduled_events_users]
-    scheduled_events = [get_additonal_information_of_event(event) for event in dict_events]
+    scheduled_events = [
+        get_additonal_information_of_event(event) for event in dict_events
+    ]
 
     return jsonify(scheduled_events), HTTPStatus.OK
 
 
+def delete_schedule(user_id, event_id):
+    try:
+        check_id_validation(user_id, User)
+        check_id_validation(event_id, Events)
+    except InvalidIdError as e:
+        return e.response, e.status_code
 
-def delete_schedule(event_id):
-    pass
+    session: Session = db.session
+
+    schedule_to_delete = (
+        session.query(Schedule).filter_by(user_id=user_id, event_id=event_id).first()
+    )
+
+    if not schedule_to_delete:
+        return {"error": "Schedule not found"}, HTTPStatus.NOT_FOUND
+
+    session.delete(schedule_to_delete)
+    session.commit()
+
+    return {"message": "Event deleted from calendar."}, HTTPStatus.OK
