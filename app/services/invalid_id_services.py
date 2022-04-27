495320b59c4efc84dad888bde1c5e9a00742d6a7
ErickDevMaster <123skatesk8@gmail.com>
2022-04-27 09:48:55 -0400

    refactor: put the model as an optional parameter

--- app/services/invalid_id_services.py
+++ app/services/invalid_id_services.py
@@ -1,14 +1,14 @@
 from app.exceptions.invalid_id_exception import InvalidIdError
 from app.configs.database import db
 
-def check_id_validation(id: str, model: db.Model):
+
+def check_id_validation(id: str, model: db.Model = None):
     if len(id) != 36:
         raise InvalidIdError(
             message={"error": f"The id {id} is not valid."}, status_code=400)
 
-    search = model.query.filter_by(id=id).first()
-    
-    if not search:
-        raise InvalidIdError(
-            message={"error": f"The id {id} is not in database."})
-    
+    if model:
+        search = model.query.filter_by(id=id).first()
+        if not search:
+            raise InvalidIdError(
+                message={"error": f"The id {id} is not in database."})
