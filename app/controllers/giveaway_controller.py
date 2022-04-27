31ce358b07954a3dba89fac83eb2db9c69759aaa
Breno Carlos <bcos182@gmail.com>
2022-04-27 02:50:46 -0300

    feat: get_giveaway function

--- app/controllers/giveaway_controller.py
+++ app/controllers/giveaway_controller.py
@@ -1,9 +1,29 @@
+from http import HTTPStatus
+
+from app.configs.database import db
+from app.models.events_model import Events
+from app.models.giveaway_model import Giveaway
+from flask import jsonify
+from sqlalchemy.orm import Query
+from sqlalchemy.orm.session import Session
+
+
 def create_giveaway():
     pass
 
 
-def get_giveaway(giveaway_id):
-    pass
+def get_giveaway(event_id: str):
+    session: Session = db.session
+
+    giveaways: Query = (
+        session.query(Giveaway)
+        .select_from(Events)
+        .join(Giveaway)
+        .filter(Events.id == event_id)
+        .all()
+    )
+
+    return jsonify(giveaways), HTTPStatus.OK
 
 
 def update_giveaway(giveaway_id):
