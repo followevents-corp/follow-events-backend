59c3c9528721b047733a1d3021fb12ef1f13f004
Breno Carlos <bcos182@gmail.com>
2022-04-28 15:45:23 -0300

    feat: updated giveaway

--- app/controllers/giveaway_controller.py
+++ app/controllers/giveaway_controller.py
@@ -75,8 +75,65 @@ def get_giveaway(event_id: str):
     return jsonify(giveaways), HTTPStatus.OK
 
 
-def update_giveaway(giveaway_id):
-    pass
+def update_giveaway(giveaway_id, event_id):
+
+    try:
+        check_id_validation(giveaway_id, Giveaway)
+    except InvalidIdError as e:
+        return e.response, e.status_code
+
+    data = request.get_json()
+
+    verified_values = incoming_values(data)
+
+    if verified_values:
+        return jsonify(verified_values), HTTPStatus.BAD_REQUEST
+
+    try:
+        valid_keys = ["name", "description", "award", "award_picture", "active"]
+
+        valid_key_types = {
+            "name": str,
+            "description": str,
+            "award": str,
+            "award_picture": str,
+            "active": bool,
+        }
+
+        verified_key = check_keys(data, valid_keys)
+
+        check_keys_type(verified_key, valid_key_types)
+
+    except MissingAttributeError as e:
+        return e.response, e.status_code
+    except AttributeTypeError as e:
+        return e.response, e.status_code
+
+    session: Session = db.session
+
+    giveaway: Query = (
+        session.query(Giveaway)
+        .select_from(Events)
+        .join(Giveaway)
+        .filter(Events.id == event_id, Giveaway.id == giveaway_id)
+        .first()
+    )
+
+    updated_giveaway = {
+        "name": data["name"],
+        "description": data["description"],
+        "award": data["award"],
+        "award_picture": data["award_picture"],
+        "active": data["active"],
+    }
+
+    for key, value in updated_giveaway.items():
+        setattr(giveaway, key, value)
+
+    session.add(giveaway)
+    session.commit()
+
+    return jsonify(giveaway), HTTPStatus.OK
 
 
 def delete_giveaway(event_id, giveaway_id):
