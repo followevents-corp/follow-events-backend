a4eb62582c16648fed4998cb0b1733a77118cdae
sarah <sarahscardini@gmail.com>
2022-04-28 21:12:46 -0300

    fix: change route name

--- app/routes/events_blueprint.py
+++ app/routes/events_blueprint.py
@@ -2,10 +2,10 @@ from flask import Blueprint
 
 from app.controllers import events_controller
 
-bp = Blueprint("user", __name__, url_prefix="/events")
+bp = Blueprint("events", __name__, url_prefix="/events")
 
 bp.get("")(events_controller.get_events)
-bp.get("<events_id>")(events_controller.get_by_id_event)
+bp.get("/<user_id>")(events_controller.get_by_id_event)
 bp.post("")(events_controller.create_event)
-bp.patch("<events_id>")(events_controller.update_event)
-bp.delete("<events_id>")(events_controller.del_event)
+bp.patch("/<events_id>")(events_controller.update_event)
+bp.delete("/<events_id>")(events_controller.del_event)
