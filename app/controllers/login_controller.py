20aa9297615cac693173f054aa712da4d04a75d4
sarah <sarahscardini@gmail.com>
2022-04-28 21:21:38 -0300

    refactor: change url name of events_url variable

--- app/controllers/login_controller.py
+++ app/controllers/login_controller.py
@@ -31,7 +31,7 @@ def login_user():
 
     access_token = create_access_token(identity=found_user.id)
     schedule_url = url_for("schedule.get_schedule", user_id=found_user.id)
-    events_url = url_for("user.get_event_by_id", user_id=found_user.id)
+    events_url = url_for("events.get_event_by_id", user_id=found_user.id)
 
     return {
         "id": found_user.id,
