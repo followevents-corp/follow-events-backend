1161f1f907fa5cb9f3f5409966801acf1b823dde
Breno Carlos <bcos182@gmail.com>
2022-04-29 16:55:42 -0300

    fix: fix merge current_id

--- app/controllers/events_controller.py
+++ app/controllers/events_controller.py
@@ -22,7 +22,7 @@ from app.services.general_services import (check_id_validation,
                                            remove_unnecessary_keys,
                                            save_changes)
 from flask import jsonify, request
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from sqlalchemy.orm.session import Session
 from werkzeug.datastructures import FileStorage
 from psycopg2.errors import ForeignKeyViolation
@@ -31,9 +31,11 @@ from sqlalchemy.exc import IntegrityError
 
 @jwt_required()
 def create_event():
+    current_user = get_jwt_identity()
+
     try:
         dict = {"name": str, "description": str, "event_date": str,
-                "event_link": str, "creator_id": str, "categories": list}
+                "event_link": str, "categories": list}
 
         files = request.files
         file = files["file"]
@@ -53,6 +55,8 @@ def create_event():
             return jsonify(verify_values), HTTPStatus.BAD_REQUEST
 
         create_categories(categories)
+        
+        new_data['creator_id'] = current_user
 
         event = Events(**new_data,  link=file)
         save_changes(event)
